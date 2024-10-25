57bbe06d77230d78db3a8ae305cb022879e3f3f2
PalakAgrawal020 <palakagrawal020@gmail.com>
2024-10-25 11:20:35 +0530

    setup shadcn ui

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
 
 function Login() {
   return (
@@ -8,6 +9,20 @@ function Login() {
             <div className='text-2xl font-semibold'>Furrniro.</div>
             <Link to="/signup">Signup</Link>
         </div>
+        <div>
+          Enter your name
+        </div>
+        <div>
+          field to enter the name
+        </div>
+        <div>
+          Enter your number
+        </div>
+        <div>
+          Field to enter the number
+        </div>
+
+        <Button>Click me</Button>
     </>
   )
 }
--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axiosInstance from '../../axios/axiosInstance';
+import { useContextProvider } from '../../Context/Context';
 
 function ProductPage() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [product, setProduct] = useState([]);
 
-
+  const { addToWishlist, addToCart } = useContextProvider();
   useEffect(() => {
     async function fetchData() {
       try {
@@ -15,18 +16,27 @@ function ProductPage() {
       } catch (error) {
         console.log("Error fetching data : ", error);
       }
-    } 
+    }
     fetchData();
-  },[id])
+  }, [id])
 
   return (
     <>
       <div className='w-56 m-10'>
-        <img src={product.image}/>
+        <img src={product.image} />
         <div>{product.title}</div>
+        <div>{product.description}</div>
+        <div>{product.price}</div>
         <button
-          className='border-2 bg-grey-100' 
+          className='border-2 bg-grey-100'
+          onClick={addToWishlist}
         >Add to Wishlist</button>
+        <button
+          className='border-2 bg-grey-100'
+          onClick={addToCart}
+        >
+          Add to Cart
+        </button>
       </div>
     </>
   )
